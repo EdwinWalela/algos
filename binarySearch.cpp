bf260dde33982d9b8d60863ad90187827365a1c5
edwinwalela <edwinwalela@gmail.com>
2019-04-13 14:36:16 +0300

    binary search iterative

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -9,7 +9,7 @@ using namespace std;
 //           M 
 
 
- int binarySearch(int arr[],int x,int left,int right){
+int binarySearchRecursive(int arr[],int x,int left,int right){
      if(left>right){
          return -1;
      }
@@ -17,20 +17,37 @@ using namespace std;
     if(arr[mid]==x){
         return mid;
     }else if(x < arr[mid]){
-        binarySearch(arr,x,left,mid-1);
+        binarySearchRecursive(arr,x,left,mid-1);
     }else if(x > arr[mid]){
-        binarySearch(arr,x,mid+1,right);
+        binarySearchRecursive(arr,x,mid+1,right);
     }
- }
+}
 
- void printArray(int arr[],int length){
+int binarySearchIterative(int arr[],int x,int n){
+     int left = 0;;
+     int right = n-1;
+
+    while(left <= right){
+        int mid = (left + right ) / 2;
+        if(arr[mid]==x){
+            return mid;
+        }else if(x < arr[mid]){
+            right = mid -1;
+        }else if(x > arr[mid]){
+            left = mid +1;
+        }
+    }
+    return -1;
+}
+
+void printArray(int arr[],int length){
     cout<<"array : [ ";
      for (int i = 0; i < length; i++){
          i != length-1 ? cout<<arr[i]<<" , " : cout<<arr[i]; 
         
      } 
      cout<<" ] "<<endl;
- }
+}
 
  int main(void){
      int arr[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17}; int x,index;
@@ -40,7 +57,11 @@ using namespace std;
 
      cout<<"\nEnter number to search for: ";
      cin>>x;
-     index =  binarySearch(arr,x,0,arrLength);
-     cout<<"\n"<<x<<" is at index : "<<index<<endl;
+     index =  binarySearchIterative(arr,x,arrLength);
+     if(index != -1){
+        cout<<"\n"<<x<<" is at index : "<<index<<endl;
+     }else{
+        cout<<"\n"<<x<<" not found"<<endl;
+     }
      return 1;
  }
